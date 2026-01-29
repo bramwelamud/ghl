f2e48e753a193e27b9990dbb17ab9532dbeff1ec
braammm well <118194115+bramwelamud@users.noreply.github.com>
2026-01-28 16:03:56 -0800

    Update listings.js

--- api/listings.js
+++ api/listings.js
@@ -46,7 +46,7 @@ export default async function handler(req, res) {
     console.log('Got token, fetching properties...');
 
     // Get properties
-    const ddfResponse = await fetch("https://ddfapi.realtor.ca/odata/v1/Property?$top=100&PropertyType=Residential&PropertyType=Multi-family", {
+    const ddfResponse = await fetch("https://ddfapi.realtor.ca/odata/v1/Property?$top=100&PropertyType=Single Family&PropertyType=Multi-family", {
       headers: { 
         'Authorization': `Bearer ${tokenData.access_token}`,
         'Accept': 'application/json',
